3152e89a0ffdadb475a83719a2dbba0bde197aad
Nikita Isupov <145199579+nifreebie@users.noreply.github.com>
2024-05-22 16:26:46 +0300

    fix

--- src/main/java/org/example/server/command_handlers/HelpCommandHandler.java
+++ src/main/java/org/example/server/command_handlers/HelpCommandHandler.java
@@ -8,6 +8,21 @@ import org.example.contract.utils.StatusCode;
 public class HelpCommandHandler extends CommandHandler<HelpCommand>{
     @Override
     public Response handle(HelpCommand command) {
-        return new ResponseWithMessage(StatusCode._200_SUCCESS_, "");
+        String helpOutput = "";
+        helpOutput += "add: добавить новый элемент в коллекцию\n" +
+                "add_if_min: добавить новый элемент в коллекцию, если его значение меньше, чем у наименьшего элемента этой коллекции\n" +
+                "clear: очистить коллекцию\n" +
+                "execute_script: считать и исполнить скрипт из указанного файла. В скрипте содержатся команды в таком же виде, в котором их вводит пользователь в интерактивном режиме.\n" +
+                "exit: завершить программу (без сохранения в файл)\n" +
+                "help: вывести справку по доступным командам\n" +
+                "info: вывести в стандартный поток вывода информацию о коллекции (тип, дата инициализации, количество элементов и т.д.)\n" +
+                "print_descending: вывести элементы коллекции в порядке убывания\n" +
+                "print_field_descending_part_number: вывести значения поля partNumber всех элементов в порядке убывания\n" +
+                "remove_by_id: удалить элемент из коллекции по его id\n" +
+                "remove_greater: удалить из коллекции все элементы, превышающие заданный\n" +
+                "remove_lower: удалить из коллекции все элементы, меньшие, чем заданный\n" +
+                "show: вывести в стандартный поток вывода все элементы коллекции в строковом представлении\n" +
+                "update: обновить значение элемента коллекции, id которого равен заданному";
+        return new ResponseWithMessage(StatusCode._200_SUCCESS_, helpOutput);
     }
 }
